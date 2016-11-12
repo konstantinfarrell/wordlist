494faa5a67dce34e007ecfa02db5c62283fec41c
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-11-12 07:17:10 -0800

    Add templates for words/home

--- wordlist/settings.py
+++ wordlist/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'wordlist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [here("templates")],
+        'DIRS': [here("templates/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
