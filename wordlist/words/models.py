5f0cf98fcf3338a133aa2fe5f77da1ffab01a178
iBrowse <dmurdabot@gmail.com>
2016-11-12 08:40:36 -0800

    Add words model.

--- wordlist/words/models.py
+++ wordlist/words/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+	"""
+	Model for a word.
+    'phonetic' contains a list of integers to be used with IPA_table
+    'language' uses 2-letter code
+	"""
+	id = models.IntegerField(primary_key=True)
+	word = models.CharField(max_length=255)
+	phonetic = models.IntegerField(unique=True)
+	part_of_speech = models.CharField(max_length=16)
+	language = models.CharField(max_length=2)
+	definition = models.CharField(max_length=255)
+	attribution = models.CharField(max_length=255)
+	notes = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "word"
+
+
+
+
