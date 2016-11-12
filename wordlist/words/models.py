33e689691bf3b1fb4d8a835c251c8dcb79fbe60a
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-11-12 08:51:22 -0800

    fix tabs

--- wordlist/words/models.py
+++ wordlist/words/models.py
@@ -2,23 +2,19 @@
 from django.db import models
 
 class Word(models.Model):
-	"""
-	Model for a word.
+    """
+    Model for a word.
     'phonetic' contains a list of integers to be used with IPA_table
     'language' uses 2-letter code
-	"""
-	id = models.IntegerField(primary_key=True)
-	word = models.CharField(max_length=255)
-	phonetic = models.IntegerField(unique=True)
-	part_of_speech = models.CharField(max_length=16)
-	language = models.CharField(max_length=2)
-	definition = models.CharField(max_length=255)
-	attribution = models.CharField(max_length=255)
-	notes = models.CharField(max_length=255)
+    """
+    id = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=255)
+    phonetic = models.IntegerField(unique=True)
+    part_of_speech = models.CharField(max_length=16)
+    language = models.CharField(max_length=2)
+    definition = models.CharField(max_length=255)
+    attribution = models.CharField(max_length=255)
+    notes = models.CharField(max_length=255)
 
     class Meta:
         db_table = "word"
-
-
-
-
