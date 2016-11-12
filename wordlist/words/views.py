ff6a2121e4f5cb55b0b35ba519d34b4772763162
iBrowse <dmurdabot@gmail.com>
2016-11-12 10:34:36 -0800

    Return all words.

--- wordlist/words/views.py
+++ wordlist/words/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from wordlist.words.models import Word
 
 
 def home(request):
-
+    """
+    """
+    words = Word.objects.all()
     return render(request, 'words/home.html', {
-
+        "words": words
     })
