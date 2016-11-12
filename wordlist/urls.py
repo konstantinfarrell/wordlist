0e01ef73cd6765d5b0e5616b51c629094efa8705
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-11-12 11:15:42 -0800

    Add basic form

--- wordlist/urls.py
+++ wordlist/urls.py
@@ -9,4 +9,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', words.home, name='home'),
+    url(r'^new/', words.create, name='words-create'),
+    url(r'^edit/(?P<word_id>\d+)/?$', words.edit, name='words-edit'),
 ]
---
+++ wordlist/words/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+from wordlist.words.models import Word
+
+
+class WordForm(forms.ModelForm):
+    """
+    Form for creating and editing a word entry.
+    """
+    class Meta:
+        model = Word
+        fields = (
+            'word',
+            'phonetic',
+            'part_of_speech',
+            'language',
+            'definition',
+            'attribution',
+            'notes'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(WordForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        """
+        """
+        cleaned_data = super(WordForm, self).clean()
+        return cleaned_data
+
+    def in_create_mode(self):
+        """
+        """
+        return self.instance.pk is None
+
+    def save(self):
+        """
+        """
+        in_create_mode = self.in_create_mode()
+        word = super(WordForm, self).save(*args, **kwargs)
+        return word
